e2c9e40d25d97011ae038dad75fad9f5170083e2
Spencer Goossens <sgoossens18@gmail.com>
2024-07-14 10:31:47 -0400

    fix: test

---
+++ src/pages/scenes/default_scene.ts
@@ -0,0 +1,6 @@
+$(document).on('knack-page-render.any', function (page, records, view, data) {
+    const urlManagement = new UrlManagement();
+    const nyResourcesTargetPageURL = 'https://drive.google.com/drive/folders/1uGgUkYLI31GQ3Ga8T8ni_Cn71vZTlEVd';
+    // to add more links jsut find the href in the inspects and call this method again with the new link
+    urlManagement.createExternalLink(nyResourcesTargetPageURL, '#ny-resources');
+});
