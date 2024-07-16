33ec2d3be0fb43dcdb5c134ba05e09bdb7b2f49d
Spencer Goossens <sgoossens18@gmail.com>
2024-07-16 19:36:57 -0400

    fix: env

--- src/pages/views/createMasterBidItem.ts
+++ src/pages/views/createMasterBidItem.ts
@@ -5,9 +5,8 @@ $(document).on("knack-view-render.view_203", function (event, scene) {
 	const nodes = document.getElementsByClassName('kn-button is-primary');
 		if (nodes.length > 0) {
 			const submitButton = nodes[0];
-			const url = submitButton.getAttribute('data-url');
 			(submitButton as any).onclick = function (e) {
-				const url = `${process.env.BASE_URL}/api/v1/projections`;
+				const url = `https://lisnda-453d0faf4382.herokuapp.com/api/v1/projections`;
 				console.log(url);
 				fetch(url, {
 					method: 'POST',
@@ -16,7 +15,8 @@ $(document).on("knack-view-render.view_203", function (event, scene) {
 					},
 					body: null
 				})
-				.then(response => response.json());
+				.then(response => response.json())
+				.catch(err => err.json());
 			};
 		}
 });
