ac481fa92257926fe8e9e99a96c54a744117f23d
Spencer Goossens <sgoossens18@gmail.com>
2024-07-17 11:11:30 -0400

    fix: dedup

--- src/components/DropdownManager.ts
+++ src/components/DropdownManager.ts
@@ -1,27 +1,33 @@
 import { UrlManager } from "./UrlManager";
 
 export interface CreateDropdownItemParams {
-    url: string;
-    siblingHref: string;
-    text: string;
+	url: string;
+	siblingHref: string;
+	text: string;
 }
 
 export class DropdownManager {
-    constructor(private readonly urlManager: UrlManager){}
+	private addedItemHeaders: String[] = [];
+	constructor(private readonly urlManager: UrlManager) {}
 
-	createDropdownItem({url, siblingHref, text}: CreateDropdownItemParams) {
-        const siblingNode = document.querySelectorAll(`a[href='${siblingHref}']`);
-        if (siblingNode?.length >= 1) {
-            const dropdownMenu = siblingNode[0].parentNode.parentNode;
-            const item = document.createElement('a.knHeader__menu-link.knHeader__menu-dropdown-link');
-            item.className = 'knHeader__menu-link knHeader__menu-dropdown-link';
-            item.textContent = `\n\n          ${text}\n\n          `;
-            item.textContent = text;
-            item.innerHTML = `<!---->\n\n          ${text}\n\n          <!---->`;
-            item.innerText = text;
-            item.textContent = text;
-            this.urlManager.createExternalLinkWithNode(url, item);
-            dropdownMenu.appendChild(item);
-        }
-    }
+	createDropdownItem({ url, siblingHref, text }: CreateDropdownItemParams) {
+		if (!this.addedItemHeaders.includes(text)) {
+			const siblingNode = document.querySelectorAll(`a[href='${siblingHref}']`);
+			if (siblingNode?.length >= 1) {
+				const dropdownMenu = siblingNode[0].parentNode.parentNode;
+				const item = document.createElement(
+					"a.knHeader__menu-link.knHeader__menu-dropdown-link"
+				);
+				item.className = "knHeader__menu-link knHeader__menu-dropdown-link";
+				item.textContent = `\n\n          ${text}\n\n          `;
+				item.textContent = text;
+				item.innerHTML = `<!---->\n\n          ${text}\n\n          <!---->`;
+				item.innerText = text;
+				item.textContent = text;
+				this.urlManager.createExternalLinkWithNode(url, item);
+				dropdownMenu.appendChild(item);
+				this.addedItemHeaders.push(text);
+			}
+		}
+	}
 }
--- src/pages/scenes/default_scene.ts
+++ src/pages/scenes/default_scene.ts
@@ -1,24 +1,30 @@
 import { UrlManager } from "src/components/UrlManager";
 import { DropdownManager } from "src/components/DropdownManager";
 
+const urlManager = new UrlManager();
+const dropdownManager = new DropdownManager(urlManager);
 
-$(document).on('knack-page-render.any', function (page, records, view, data) {
-    const urlManager = new UrlManager();
-    const dropdownManager = new DropdownManager(urlManager);
-    const nyResourcesTargetPageURL = 'https://drive.google.com/drive/folders/1uGgUkYLI31GQ3Ga8T8ni_Cn71vZTlEVd';
-    // to add more links just find the href in the inspects and call this method again with the new link
-    urlManager.createExternalLink(nyResourcesTargetPageURL, '#ny-resources');
+$(document).on("knack-page-render.any", function (page, records, view, data) {
+	const nyResourcesTargetPageURL =
+		"https://drive.google.com/drive/folders/1uGgUkYLI31GQ3Ga8T8ni_Cn71vZTlEVd";
+	// to add more links just find the href in the inspects and call this method again with the new link
+	urlManager.createExternalLink(nyResourcesTargetPageURL, "#ny-resources");
 
-    const dropDownToUrlMap = new Map<string,string>();
-    dropDownToUrlMap.set('LISNDA Group Docs', 'https://drive.google.com/drive/folders/1HeLwhf4XbfAQu5z3cnXfoVtDbstbWT0z');
-    dropDownToUrlMap.set('LISNDA Recipe Sharing Folder', 'https://drive.google.com/drive/folders/14Qs0wdZs_AgqRn1oryT1pEOuvXXJq352');
+	const dropDownToUrlMap = new Map<string, string>();
+	dropDownToUrlMap.set(
+		"LISNDA Group Docs",
+		"https://drive.google.com/drive/folders/1HeLwhf4XbfAQu5z3cnXfoVtDbstbWT0z"
+	);
+	dropDownToUrlMap.set(
+		"LISNDA Recipe Sharing Folder",
+		"https://drive.google.com/drive/folders/14Qs0wdZs_AgqRn1oryT1pEOuvXXJq352"
+	);
 
-    dropDownToUrlMap.forEach((v, k) => {
-        dropdownManager.createDropdownItem({
-            url: v,
-            siblingHref: '#ny-resources',
-            text: k
-        });
-    });
-
+	dropDownToUrlMap.forEach((v, k) => {
+		dropdownManager.createDropdownItem({
+			url: v,
+			siblingHref: "#ny-resources",
+			text: k,
+		});
+	});
 });
