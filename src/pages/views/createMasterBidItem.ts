258c75511e19ab61c4338b3f8a44b0538c876608
Spencer Goossens <sgoossens18@gmail.com>
2024-07-17 08:30:02 -0400

    fix: get mbi

--- src/pages/views/createMasterBidItem.ts
+++ src/pages/views/createMasterBidItem.ts
@@ -1,22 +1,31 @@
 /**
  *  - any
  */
-$(document).on("knack-view-render.view_203", function (event, scene) {
-	const nodes = document.getElementsByClassName('kn-button is-primary');
-		if (nodes.length > 0) {
-			const submitButton = nodes[0];
-			(submitButton as any).onclick = function (e) {
-				const url = `${process.env.BASE_URL}/api/v1/projections`;
-				console.log(url);
-				fetch(url, {
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json'
-					},
-					body: null
-				})
-				.then(response => response.json())
-				.catch(err => err.json());
-			};
-		}
+$(document).on("knack-view-render.view_203", function (event, view, data) {
+	$("#view_203 .kn-button").on("click", function (e) {
+		e.preventDefault(); // Prevent the default form submission
+		e.stopPropagation(); // Prevent the click event from bubbling up
+		const request = {
+			itemName: $(`input#field_171`).val(),
+			bidName: $(`input#field_179_raw`).val(),
+			projectionUnit: $(`input#field_405`).val(),
+			bidUnit: $(`input#field_173`).val(),
+			bidOrProjUnit: $(`input#field_174`).val(),
+			bidSpecification: $(`input#field_175`).val(),
+			divertedCommodities: $(`input#field_319`).val(),
+			minProjection: $(`input#field_191`).val(),
+			isActive: $(`input#field_180`).val(),
+		};
+		console.log({ data, event, name: $(`input#field_171`).val() });
+		const url = `${process.env.BASE_URL}/api/v1/projections`;
+		fetch(url, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: null,
+		})
+			.then((response) => response.json())
+			.catch((err) => err.json());
+	});
 });
