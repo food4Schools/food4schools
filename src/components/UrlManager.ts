5e7b9d3f7b9d8fb71f8fe7d14a16f7082641ab61
Spencer Goossens <sgoossens18@gmail.com>
2024-10-12 08:19:01 -0400

    fix: broken links

--- src/components/UrlManager.ts
+++ src/components/UrlManager.ts
@@ -1,14 +1,17 @@
 export class UrlManager {
     createExternalLink(url: string, href: String) {
-        const node = document.querySelectorAll(`a[href='${href}']`);
-        if (node.length > 0) {
-			const el = node[0];
-            (el as HTMLAnchorElement).onclick = function (e) {
-                e.preventDefault(); // prevent the knack click
-                e.stopPropagation(); // Prevent the click event from bubbling up
-                window.open(url, '_blank');
-            };
-        }
+        var checkForNode = setInterval(function() {
+            const node = document.querySelectorAll(`a[href='${href}']`);
+            if (node.length > 0) {
+                clearInterval(checkForNode);  // Stop checking once the element is found
+                const el = node[0];
+                (el as HTMLAnchorElement).onclick = function (e) {
+                    e.preventDefault(); // prevent the knack click
+                    e.stopPropagation(); // Prevent the click event from bubbling up
+                    window.open(url, '_blank');
+                };
+            }
+        }, 100);
     }
 
     createExternalLinkWithNode(url: string, node: HTMLElement) {
