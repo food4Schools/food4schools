6880245d4309a0f933ec584daa5ad9928441eb40
Spencer Goossens <sgoossens18@gmail.com>
2024-10-12 08:14:35 -0400

    fix: pagination

--- src/pages/scenes/default_scene.ts
+++ src/pages/scenes/default_scene.ts
@@ -4,6 +4,15 @@ import { DropdownManager } from "src/components/DropdownManager";
 const urlManager = new UrlManager();
 const dropdownManager = new DropdownManager(urlManager);
 
+interface View {
+	key: string;
+	type: string;
+	'results_type': string;
+}
+interface TableView extends View {
+	'rows_per_page': number;
+}
+
 const dropDownToUrlMap = new Map<string, string>();
 dropDownToUrlMap.set(
 	"LISNDA Group Docs",
@@ -25,4 +34,20 @@ $(document).on("knack-page-render.any", function (page, records, view, data) {
 		siblingData: Array.from(dropDownToUrlMap.entries()).map(([k, v]) => ({ url: v, text: k })),
 	});
 
+
+	// Use setInterval to continuously check if the select[name="limit"] element exists
+	console.log({page, records, view, data})
+	const tableViews = (records.views as View[]).filter((v) => v.type === 'table' || v.results_type === 'table');
+	const searchTableViews = (records.views as View[]).filter((v) => v.results_type === 'table');
+	const nonTableViews =(records.views as View[]).filter((v) => v.type !== 'table');
+	// If the select element exists, set the value and stop the interval
+	if (tableViews.length > 0) {
+		const newTableViews = tableViews.map((tv) => ({...tv, 'rows_per_page': 1000}));
+		const newSearchTableViews = searchTableViews.map((tv) => ({...tv, 'rows_per_page': '1000'}));
+		records.views = nonTableViews.concat([...newTableViews,...newSearchTableViews]);
+	}
+	console.log({page, records, view, data})
 });
+
+
+
