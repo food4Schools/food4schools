2f6d484d59b39b5c0e501ead42cdd398a6b3a8b6
Spencer Goossens <sgoossens18@gmail.com>
2025-02-14 15:45:11 -0500

    fix: urls

--- src/pages/scenes/default_scene.ts
+++ src/pages/scenes/default_scene.ts
@@ -1,32 +1,18 @@
 import { UrlManager } from "src/components/UrlManager";
-import { DropdownManager } from "src/components/DropdownManager";
-import { ItemCountSelector } from "src/components/ItemCountSelector";
 
 const urlManager = new UrlManager();
-const dropdownManager = new DropdownManager(urlManager);
-const itemCountSelector = new ItemCountSelector();
-
-const dropDownToUrlMap = new Map<string, string>();
-dropDownToUrlMap.set(
-	"LISNDA Group Docs",
-	"https://drive.google.com/drive/folders/1HeLwhf4XbfAQu5z3cnXfoVtDbstbWT0z"
-);
-dropDownToUrlMap.set(
-	"LISNDA Recipe Sharing Folder",
-	"https://drive.google.com/drive/folders/14Qs0wdZs_AgqRn1oryT1pEOuvXXJq352"
-);
 
 $(document).on("knack-page-render.any", function (page, records, view, data) {
-	const nyResourcesTargetPageURL =
-		"https://drive.google.com/drive/folders/1S1XcIYHBNczp4XeB7qLQHWbFiy6qsSDF";
-	// to add more links just find the href in the inspects and call this method again with the new link
-	urlManager.createExternalLink(nyResourcesTargetPageURL, "#ny-resources");
-
-	dropdownManager.createDropdownItems({
-		siblingHref: "#ny-resources",
-		siblingData: Array.from(dropDownToUrlMap.entries()).map(([k, v]) => ({
-			url: v,
-			text: k,
-		})),
-	});
+	urlManager.createExternalLink(
+		"https://drive.google.com/drive/folders/1S1XcIYHBNczp4XeB7qLQHWbFiy6qsSDF",
+		"#ny-resources"
+	);
+	urlManager.createExternalLink(
+		"https://drive.google.com/drive/folders/1HeLwhf4XbfAQu5z3cnXfoVtDbstbWT0z",
+		"#lisnda-group-docs"
+	);
+	urlManager.createExternalLink(
+		"https://drive.google.com/drive/folders/14Qs0wdZs_AgqRn1oryT1pEOuvXXJq352",
+		"#lisnda-recipe-sharing-folder"
+	);
 });
